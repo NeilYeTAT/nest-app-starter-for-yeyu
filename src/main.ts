6629d3aa6a52da8bbcc33e55d4eac85bb0efd5e9
Ye Yu <nearjilt@gmail.com>
2025-12-22 20:43:50 +0800

    chore: 🪛 replace express with fastify and update bootstrap configuration

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
+import type { NestFastifyApplication } from '@nestjs/platform-fastify'
 import { NestFactory } from '@nestjs/core'
+import { FastifyAdapter } from '@nestjs/platform-fastify'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), {
+    logger: ['error', 'warn'],
+  })
+
+  app.setGlobalPrefix('api')
+
+  const PORT = process.env.PORT ?? 4040
+
+  await app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Nest app is running on http://localhost:${PORT}/api`)
+  })
 }
 
 bootstrap()
